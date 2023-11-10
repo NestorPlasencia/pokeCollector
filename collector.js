5bc65b9dba2d3c4be59fcce3c133a0d9e687c0cc
Nestor Plasencia <nplasencia82@gmail.com>
2023-11-09 22:28:29 -0500

    Update collector.js

--- collector.js
+++ collector.js
@@ -22,18 +22,19 @@ class Collector {
     Collector._COLLECTOR_USER_ID = collector_user_id;
   }
 
-  static _COLLECTIONS_LIST = [];
+  static __COLLECTIONS_LIST = [];
 
-  static get COLLECTIONS_LIST() {
-    return Collector._COLLECTIONS_LIST;
+  static get _COLLECTIONS_LIST() {
+    return Collector.__COLLECTIONS_LIST;
+  }
+  _;
+
+  static set __COLLECTIONS_LIST(_collections_list) {
+    Collector.__COLLEC_TIONS_LIST = _collections_list;
   }
 
-  static set COLLECTIONS_LIST(collections_list) {
-    Collector._COLLECTIONS_LIST = collections_list;
-  }
-
-  static _SETS_LIST = [];
-
+  static _SET_S_LIST = [];
+  _;
   static get SETS_LIST() {
     return Collector._SETS_LIST;
   }
@@ -172,11 +173,11 @@ class Collector {
 
   // Carga la propiedad Collections List
   static loadCollections = async () => {
-    Collector._COLLECTIONS_LIST = await Collector.getCollectionsList();
+    Collector.__COLLECTIONS_LIST = await Collector.getCollectionsList();
   };
 
   // Trae todas las cartas de una colección
-  static getCardsFromCollection = async (collectionId) => {
+  static getCar_dsFromCollection = async (collectionId) => {
     const url = `${Collector.COLLECTOR_API_URL}/collections/${Collector.COLLECTOR_USER_ID}/products?searchString=&offset=0&limit=10000&filters=&sortType=&sortOrder=&groupId=&collectionId=${collectionId}`;
     const collection = await Collector.getData(url);
     return Collector.convertArrKeysToNumber(collection.data);
@@ -214,6 +215,49 @@ class Collector {
     return cards;
   };
 
+  // Obtiene el Collection Id de un Collection Name
+  static getCollectionIdFromCollectionName = (collectionName) => {
+    return Collector._COLLECTIONS_LIST.find((c) => c.name == collectionName).id;
+  };
+
+  // Obtiene los Collection Ids de varios Collection Names
+  static getCollectionIdsFromCollectionNames = (collectionsNames) => {
+    return collectionsNames.map((n) =>
+      Collector.getCollectionIdFromCollectionName(n)
+    );
+  };
+
+  // Obtiene las cartas de un Collection Name
+  static getCardsFromCollectionName = async (collectionName) => {
+    const collectionId =
+      Collector.getCollectionIdFromCollectionName(collectionName);
+    const collectionCards = await Collector.getCardsFromCollection(
+      collectionId
+    );
+    return collectionCards;
+  };
+
+  // Obtiene las cartas de varios Collection Names
+  static getCardsFromCollectionNames = async (collectionNames) => {
+    const collectionsIds =
+      Collector.getCollectionIdsFromCollectionNames(collectionNames);
+    const collectionsCards = await Collector.getCollectionsCardsById(
+      collectionsIds
+    );
+    return collectionsCards;
+  };
+
+  // Obtiene varias cartas de un Collection Names
+  static getCardsByIdInCollectionName = async (cardsId, collectionName) => {
+    const collectionId =
+      Collector.getCollectionIdFromCollectionName(collectionName);
+    const collectionCards = await Collector.getCardsInCollectionId(
+      cardsId,
+      collectionId
+    );
+    return collectionCards;
+  };
+
   // AGREGAR O ASIGNAR CARTAS A COLECCIONES
 
   // Asigna una cantidad determinada de una carta de determinado tipo a una colección
@@ -244,19 +288,6 @@ class Collector {
       1
     );
     return response;
-  };
-
-  // Regresa la cantidad de un tipo de una carta en una colección
-  static getQuantityFromCard = (card, subType) => {
-    let quantity = 0;
-    if (card.ungraded_sub_types != 0) {
-      card.ungraded_sub_types.forEach((card) => {
-        if (card.product_sub_type === subType) {
-          quantity = card.quantity;
-        }
-      });
-    }
-    return quantity;
   };
 
   // Agrega una cantidad determinada de una carta de determinado tipo a las ya existes en una colección
@@ -310,6 +341,117 @@ class Collector {
     const setsCards = setsArrays.flat();
     return setsCards;
   };
+
+  // Obtiene el Set Id del Set Name
+  static getSetIdFromSetName = (setName) => {
+    return Collector._SETS_LIST.find((s) => s.catalog_group_name == setName)
+      .catalog_group_id;
+  };
+
+  // Obtiene los Set Ids de los Set Names
+  static getSetIdsFromSetNames = (setsNames) => {
+    return setsNames.map((n) => Collector.getSetIdFromSetName(n));
+  };
+
+  // Obtiene las Cartas de un Set Name
+  static getCardsFromSetName = async (setName) => {
+    const setId = Collector.getSetIdFromSetName(setName);
+    const setCards = await Collector.getCardsFromSet(setId);
+    return setCards;
+  };
+
+  // Obtiene las Cartas de varios Set Names
+  static getCardsFromSetNames = async (setsNames) => {
+    const setsIds = Collector.getSetIdsFromSetNames(setsNames);
+    const setsCards = await Collector.getSetsCardsById(setsIds);
+    return setsCards;
+  };
+
+  // PRINT FORMATS
+
+  // Obtiene un array con los ids de un array de cartas
+  static extractCardsIds = (cards) => {
+    return cards.map((p) => p.product_id);
+  };
+
+  // Imprime los ids de las cartas separados por comas
+  static printCardsIds = (cards) => {
+    console.log(Collector.extractCardsIds(cards).join(","));
+  };
+
+  // Imprime nombre y la coleccion de un array de cartas
+  static printCardsNameAndCollection = (cards) => {
+    console.log(cards.map((c) => `${c.product_name} ${c.catalog_group}`));
+  };
+
+  // Imprime una carta en su formato de massEntry
+  static printCardMassEntryFormat = (card) => {
+    console.log(
+      `1 ${card.product_name}${Collector.convertSetName(
+        card.catalog_group,
+        "CollectrSetName",
+        "TCGPlayerCode"
+      )} ${card.market_price}`
+    );
+  };
+
+  // Imprime una carta en su formato de massEntry
+  static printCardsMassEntryFormat = (cards) => {
+    cards.forEach((c) => Collector.printCardMassEntryFormat(c));
+  };
+
+  // UTILIDADES DE CARTAS
+
+  static NORMAL_SEMI_SET_SUBTYPE = ["Normal", "Holofoil"];
+
+  static REVERSE_SEMI_SET_SUBTYPE = ["Reverse Holofoil"];
+
+  static SEMI_SETS_RARITIES = [
+    "Common",
+    "Uncommon",
+    "Rare",
+    "Holo Rare",
+    "Unconfirmed",
+  ];
+
+  // Devuelve verdadero si la carta tiene un subtipo Normal
+  static isNormalCard = (card) => {
+    return card.ungraded_sub_types.some((subType) =>
+      Collector.NORMAL_SEMI_SET_SUBTYPE.includes(subType.product_sub_type)
+    );
+  };
+
+  // Devuelve verdadero si la carta tiene un subtipo Reverse
+  static isReverseCard = (card) => {
+    return card.ungraded_sub_types.some((subType) =>
+      Collector.REVERSE_SEMI_SET_SUBTYPE.includes(subType.product_sub_type)
+    );
+  };
+
+  // Devuelve verdadero si la carta es de semiSet
+  static isSemiSetCard = (card) => {
+    return Collector.SEMI_SETS_RARITIES.includes(card.rarity);
+  };
+
+  // Devuelve verdadero si la carta es un Hit
+  static isHitCard = (card) => {
+    return !Collector.isSemiSetCard(card);
+  };
+
+  // Regresa la cantidad de un subtipo de una carta en una colección
+  static getQuantityFromCard = (card, subType) => {
+    let quantity = 0;
+    if (card.ungraded_sub_types != 0) {
+      card.ungraded_sub_types.forEach((card) => {
+        if (card.product_sub_type === subType) {
+          quantity = card.quantity;
+        }
+      });
+    }
+    return quantity;
+  };
+
+  // SETS NAMES
 
   static _SET_NAMES_DICTIONARY = [
     {
@@ -527,6 +669,14 @@ class Collector {
   static get SET_NAMES_DICTIONARY() {
     return Collector._SET_NAMES_DICTIONARY;
   }
+
+  // Convierte el código de un set de un formato a otro
+  static convertSetName = (code, keySearch, keyReturn) => {
+    const codeFind = Collector._SET_NAMES_DICTIONARY.find(
+      (set) => set[keySearch] == code
+    );
+    return codeFind ? codeFind[keyReturn] : null;
+  };
 }
 
 // Exporta la clase para que pueda ser utilizada fuera de la librería
